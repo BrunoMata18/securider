8f3340b3ee843de74cccbf9fbc4a2754d6e61275
brunomata18 <brunomiguel660@gmail.com>
2023-03-10 08:53:41 +0000

    bd online

--- models/connection.js
+++ models/connection.js
@@ -1,13 +1,11 @@
 var mysql = require('mysql');
 var conn = mysql.createPool({
-    host: "localhost",
-    user: "server",
-    password: "123456789",
-    database : 'securiderdb',
+    host: "us-cdbr-east-06.cleardb.net",
+    user: "b734c6d729c6c2",
+    password: "2b006546",
+    database : 'heroku_f4abcce1d68c357',
     port: '3306'
 });
 
 
 module.exports = conn;
-
-
