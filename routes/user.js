c8f966368a0730c4b0698a24298486b0d5da454f
miguelcruz <miguelcruzcolegio@gmail.com>
2023-03-11 03:46:48 +0000

    d

--- routes/user.js
+++ routes/user.js
@@ -164,6 +164,24 @@ const updateuser = (request, response) => {
   }
 }
 
+const getutilizadorId = (req, res) => {
+  const userId = req.params.id; // obtém o ID do usuário a partir da URL
+
+  try {
+    client.query('SELECT * FROM users WHERE id = $1', [userId], (error, results) => {
+      if (error) {
+        throw error;
+      }
+
+      res.status(200).json(results.rows[0]);
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json("Erro ao obter o usuário");
+  } finally {
+    console.log("Sucesso!");
+  }
+};
 
 
 
@@ -173,5 +191,6 @@ module.exports = {
   createutilizador,
   userdelete,
   updateuser,
-  getutilizadortipo
+  getutilizadortipo,
+  getutilizadorId
 }
