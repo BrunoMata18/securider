6e03a638ac21fccc4e632852fd79b9ccf6187f4b
Miguelkrugi <miguelcruzcolegio@gmail.com>
2023-05-23 17:18:23 +0100

    s

--- routes/user.js
+++ routes/user.js
@@ -637,7 +637,7 @@ const createtrilhaadquirida = (request, response) => {
     const trilha_adquirida = request.body
     const trilha_completada = false;
 
-    console.log(trilha)
+    console.log(trilha_adquirida)
     const query = 'INSERT INTO trilha_adquirida (trilha_completada, trilha_adquirida_uti_id, trilha_adquirida_trilha_id) VALUES ('+ trilha_completada +', '+ trilha_adquirida.trilha_adquirida_uti_id + ' , ' + trilha_adquirida.trilha_adquirida_trilha_id +")'";
 
     console.log(query)
@@ -649,7 +649,7 @@ const createtrilhaadquirida = (request, response) => {
     })
   } catch (e) {
     console.log(e);
-    response.status(500).json({error: console.log(e)})
+    response.status(500).json({error: "Erro"})
   } finally {
     console.log("success");
   }
